44c40db1b6d1b650ed7120cc64c483842813c89a
Praveen Lodhi <praveenlodhi.official@gmail.com>
2026-02-06 15:12:49 +0000

    feat(admin): create shadcn admin dashboard

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,10 +1,28 @@
-import { cn } from "@/lib/utils";
+import { AppSidebar } from "@/components/admin/app-sidebar";
+import { SiteHeader } from "@/components/admin/site-header";
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 
-type AdminLayoutProps = {
-  children: React.ReactNode;
-  className?: string;
-};
-
-export default function AdminLayout({ children, className }: AdminLayoutProps) {
-  return <div className={cn(className)}>{children}</div>;
+export default function Page({ children }: { children: React.ReactNode }) {
+  return (
+    <SidebarProvider
+      style={
+        {
+          "--sidebar-width": "calc(var(--spacing) * 72)",
+          "--header-height": "calc(var(--spacing) * 12)",
+        } as React.CSSProperties
+      }
+    >
+      <AppSidebar variant="inset" />
+      <SidebarInset>
+        <SiteHeader />
+        <div className="flex flex-1 flex-col">
+          <div className="@container/main flex flex-1 flex-col gap-2">
+            <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+              {children}
+            </div>
+          </div>
+        </div>
+      </SidebarInset>
+    </SidebarProvider>
+  );
 }
